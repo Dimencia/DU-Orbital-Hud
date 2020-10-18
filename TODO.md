a71503eeb8178198dcbe31df5cb8aff6023a9856
Cliff Hudson <cliff.s.hudson@gmail.com>
2020-10-18 10:54:20 -0700

    Extract version number from lua

--- TODO.md
+++
@@ -1,6 +0,0 @@
-* Wrap+Minify seems to break the pre-processor in the auto-configure - it will not do the replace on L_TEXT to grab localized text in the hud.
-* Exported variables (script parameters) are no longer visible outside of the script so users cannot configure it.
-
-    
-
-
--- wrap.sh
+++ wrap.sh
@@ -2,6 +2,8 @@
 
 # Extract the exports because the minifier will eat them.
 grep "\-- export:" ButtonHUD.lua | sed -e 's/^[ \t]*/        /' -e 's/-- export:/--export:/' > ButtonHUD.exports
+VERSION_NUMBER=`grep "VERSION_NUMBER = .*" ButtonHUD.lua | sed -E "s/\s*VERSION_NUMBER = (.*)/\1/"`
+
 sed "/-- export:/d" ButtonHUD.lua > ButtonHUD.extracted.lua
 
 # Minify the lua
@@ -12,7 +14,7 @@ else
 fi
 
 # Wrap in AutoConf
-lua wrap.lua --handle-errors --output yaml --name 'ButtonsHud - Dimencia and Archaegeo v4.60 (Minified)' ButtonHUD.min.lua ButtonHUD.wrapped.conf --slots core:class=CoreUnit radar:class=RadarPVPUnit,select=manual antigrav:class=AntiGravityGeneratorUnit warpdrive:class=WarpDriveUnit gyro:class=GyroUnit weapon:class=WeaponUnit,select=manual dbHud:class=databank vBooster:class=VerticalBooster hover:class=Hovercraft door:class=DoorUnit,select=manual forcefield:class=ForceFieldUnit,select=manual atmofueltank:class=AtmoFuelContainer,select=manual spacefueltank:class=SpaceFuelContainer,select=manual rocketfueltank:class=RocketFuelContainer,select=manual
+lua wrap.lua --handle-errors --output yaml --name "ButtonsHud - Dimencia and Archaegeo v$VERSION_NUMBER (Minified)" ButtonHUD.min.lua ButtonHUD.wrapped.conf --slots core:class=CoreUnit radar:class=RadarPVPUnit,select=manual antigrav:class=AntiGravityGeneratorUnit warpdrive:class=WarpDriveUnit gyro:class=GyroUnit weapon:class=WeaponUnit,select=manual dbHud:class=databank vBooster:class=VerticalBooster hover:class=Hovercraft door:class=DoorUnit,select=manual forcefield:class=ForceFieldUnit,select=manual atmofueltank:class=AtmoFuelContainer,select=manual spacefueltank:class=SpaceFuelContainer,select=manual rocketfueltank:class=RocketFuelContainer,select=manual
 
 # Re-insert the exports
 sed '/script={}/e cat ButtonHUD.exports' ButtonHUD.wrapped.conf > ButtonHUD.conf
