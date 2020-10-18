d7439cab901e621fe3f0f779b92613312283300e
Cliff Hudson <cliff.s.hudson@gmail.com>
2020-10-18 14:58:26 -0700

    Move ButtonHUD.lua to src

--- scripts/wrap.sh
+++ scripts/wrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # We expect this file to be run from the <repo>/scripts directory
-LUA_SRC=../ButtonHUD.lua
+LUA_SRC=../src/ButtonHUD.lua
+CONF_DST=../ButtonHUD.conf
 mkdir -p work
 
 # Extract the exports because the minifier will eat them.
@@ -20,7 +21,7 @@ fi
 lua wrap.lua --handle-errors --output yaml --name "ButtonsHud - Dimencia and Archaegeo v$VERSION_NUMBER (Minified)" work/ButtonHUD.min.lua work/ButtonHUD.wrapped.conf --slots core:class=CoreUnit radar:class=RadarPVPUnit,select=manual antigrav:class=AntiGravityGeneratorUnit warpdrive:class=WarpDriveUnit gyro:class=GyroUnit weapon:class=WeaponUnit,select=manual dbHud:class=databank vBooster:class=VerticalBooster hover:class=Hovercraft door:class=DoorUnit,select=manual forcefield:class=ForceFieldUnit,select=manual atmofueltank:class=AtmoFuelContainer,select=manual spacefueltank:class=SpaceFuelContainer,select=manual rocketfueltank:class=RocketFuelContainer,select=manual
 
 # Re-insert the exports
-sed '/script={}/e cat work/ButtonHUD.exports' work/ButtonHUD.wrapped.conf > ../ButtonHUD.conf
+sed '/script={}/e cat work/ButtonHUD.exports' work/ButtonHUD.wrapped.conf > $CONF_DST
 
 # Fix up minified L_TEXTs which requires a space after the comma
-sed -i -E 's/L_TEXT\(("[^"]*"),("[^"]*")\)/L_TEXT(\1, \2)/g' ../ButtonHUD.conf
+sed -i -E 's/L_TEXT\(("[^"]*"),("[^"]*")\)/L_TEXT(\1, \2)/g' $CONF_DST
