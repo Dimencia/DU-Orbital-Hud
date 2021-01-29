038ef7e3c04d41ac5ea042d61a0e898003540e7b
Dimencia <Dimencia@gmail.com>
2021-01-29 14:20:27 -0500

    version number

--- src/ButtonHUD.lua
+++ src/ButtonHUD.lua
@@ -5015,7 +5015,7 @@ end
 
 -- Start of actual HUD Script. Written by Dimencia and Archaegeo. Optimization and Automation of scripting by ChronosWS  Linked sources where appropriate, most have been modified.
 function script.onStart()
-    VERSION_NUMBER = 4.933
+    VERSION_NUMBER = 4.934
     SetupComplete = false
     beginSetup = coroutine.create(function()
         Nav.axisCommandManager:setupCustomTargetSpeedRanges(axisCommandId.longitudinal,
