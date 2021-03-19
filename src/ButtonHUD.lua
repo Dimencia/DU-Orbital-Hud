3b624b3aeaf276743c7955365827c6d5ff9b131f
Archaegeo <archaegeo.reg@gmail.com>
2021-03-19 12:33:15 -0400

    Update version number

--- src/ButtonHUD.lua
+++ src/ButtonHUD.lua
@@ -5584,7 +5584,7 @@ end
 -- Start of actual HUD Script. Written by Dimencia and Archaegeo. Optimization and Automation of scripting by ChronosWS  Linked sources where appropriate, most have been modified.
 
 function script.onStart()
-    VERSION_NUMBER = 5.451
+    VERSION_NUMBER = 5.450
     SetupComplete = false
     beginSetup = coroutine.create(function()
         Nav.axisCommandManager:setupCustomTargetSpeedRanges(axisCommandId.longitudinal,
